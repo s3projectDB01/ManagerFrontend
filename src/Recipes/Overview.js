1af0058d821e65ad849f26753113a7f4336806e3
I461268 <d.picus@student.fontys.nl>
2021-05-11 15:02:17 +0200

    remove recipe

--- src/Recipes/Overview.js
+++ src/Recipes/Overview.js
@@ -1,10 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
 export function Overview(){
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
+        fetch("https://localhost:5001/Recipe/getRecipes")
+        .then(results => results.json())
+        .then(res =>{
+            setRecipes(res);
+        });
+    }, []);
+
+    function OnDelete(title){
+        fetch(`https://localhost:5001/Recipe/delete/${title}`, { method: 'DELETE'})
+    }
+        
 
     return(
         <center>
-            <div>             
-                <h2>Overview</h2>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Recipes</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recipes.map(recipe =>
+                        <tr key ={recipe}>
+                            <td>{recipe.title}</td>
+                            <td><button onClick={() => OnDelete(recipe.title)}>delete</button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </center>    
     );
 }
